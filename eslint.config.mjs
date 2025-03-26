57cc5d06b94480674b4393cb29d7dc2ad253b975
elena-lucky <elena.ivanovada@gmail.com>
2025-03-26 15:35:29 +0300

    chore: add plugin simple import sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,24 @@
 import globals from 'globals';
+import js from '@eslint/js'
 import nextPlugin from '@next/eslint-plugin-next';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  { ignores: ['.next/**', 'node_modules/**'] },
-  
+  { ignores: ['.next/**', 'node_modules/**', 'eslint.config.mjs'] },
+  { files: ['**/*.{ts,tsx}'] },
   {
     languageOptions: {
       globals: {
@@ -29,34 +32,38 @@ const eslintConfig = [
       },
     },
   },
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "prettier"),
-    {
+ ...compat.extends(
+  'next/core-web-vitals',
+  'next/typescript',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:react/recommended',
+  'prettier',
+),
+  {
     plugins: {
       '@next/next': nextPlugin,
       'react-hooks': pluginReactHooks,
-      'react': pluginReact,
+      react: pluginReact,
+      'simple-import-sort': pluginSimpleImportSort,
     },
     rules: {
-       'react/react-in-jsx-scope': 'off',
+      'react/react-in-jsx-scope': 'off',
       'react/jsx-key': 'error',
       '@typescript-eslint/no-unused-vars': [
         'warn',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
       ],
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/ban-ts-comment': [
         'error',
-        { 'ts-expect-error': 'allow-with-description' }
+        { 'ts-expect-error': 'allow-with-description' },
       ],
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       '@next/next/no-html-link-for-pages': 'error',
-       "no-console": ["error", { allow: ["warn", "error", "info"] }]
+      'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
 ];
