76ba11fed03553be54619739bb52d889d588f096
Nika Verman <nikaverman@gmail.com>
2025-04-01 22:32:57 +0300

    fix: update prettify

--- src/hooks/useJsonFormatter.ts
+++ src/hooks/useJsonFormatter.ts
@@ -3,31 +3,17 @@ import estreePlugin from 'prettier/plugins/estree';
 import prettier from 'prettier/standalone';
 
 export const useJsonFormatter = () => {
-  const validateJSON = (input: string) => {
-    try {
-      JSON.parse(input);
-      return true;
-    } catch {
-      return false;
-    }
-  };
-
   const formatJson = async (input: string) => {
-    if (!validateJSON(input)) {
-      // TODO: ADD TOASTER
-      throw new Error('Invalid JSON format');
-    }
-
     try {
       return await prettier.format(input, {
         parser: 'json',
         plugins: [babelPlugin, estreePlugin],
       });
-    } catch (_e) {
-      // TODO: ADD TOASTER
-      throw new Error('Formatting failed');
+    } catch {
+      // TODO: show TOAST
+      return input;
     }
   };
 
-  return { formatJson, validateJSON };
+  return { formatJson };
 };
