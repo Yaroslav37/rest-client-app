4a016372552bb3e408ee3b209f05031677115323
Nika Verman <nikaverman@gmail.com>
2025-04-19 16:31:15 +0300

    feat: add some fixes

--- src/components/ui/Spinner/Spinner.tsx
+++ src/components/ui/Spinner/Spinner.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 
 export const Spinner = () => {
   return (
-    <div
-      data-testid="spinner"
-      className="fixed flex justify-center items-center inset-0 bg-[rgba(0,0,0,0.1)]"
-    >
-      <Image width={80} height={80} src="/spinner.svg" alt="spinner" priority />
+    <div className="relative h-screen w-full">
+      <div
+        data-testid="spinner"
+        className="absolute flex justify-center items-center inset-0 bg-[rgba(0,0,0,0.1)]"
+      >
+        <Image width={80} height={80} src="/spinner.svg" alt="spinner" priority />
+      </div>
     </div>
   );
 };
